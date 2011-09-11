972415f6a49ad46504b83d765be4dd50e4056ae8
Sasa Ostrouska <casaxa@gmail.com>
2011-09-11 12:50:10 -0300

    Moved the usage() from main.c into its proper file named usage.c and added it to the Makefile.

---
+++ usage.c
@@ -0,0 +1,11 @@
+/* This file is part of the hello_world program.
+ */
+#include "main.h"
+
+void usage (char *pname)
+{
+	printf ("Hi user :) \n");
+	printf ("To run this program you must suply one argument.\n");
+	printf ("Something like this:\n");
+	printf ("%s: your_name\n", pname);
+}
