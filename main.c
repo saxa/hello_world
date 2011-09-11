67a32b88fba379689c32b9281591237de6ffe204
Sasa Ostrouska <casaxa@gmail.com>
2011-09-11 00:40:31 -0300

    Added a simple non working function.

--- main.c
+++ main.c
@@ -5,13 +5,25 @@
 
 #include <stdio.h>
 
-int main (int argv, char **argc)
+void usage (char *pname);
+
+int main (int argc, char **argv)
 {
+	/* Lets see if we are run with args */
+	if ( argc=0 )
+		usage (*argv);
+
 	int i = 0;
 
 	for (i=0; i<10; i++)
-		printf ("%s\n", argc[1]);
+		printf ("%s\n", argv[1]);
 
 	return 0;
 }
-
+void usage (char *progname)
+{
+	printf ("Hi user :) \n \
+		To run this program you must suply one argument.\n \
+		Something like this:\n \
+		%s: your_name\n", *progname);
+}
